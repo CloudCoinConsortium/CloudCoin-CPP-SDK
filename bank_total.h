e9e3a77e8fa3fdbea309dd48d6b77d2548622b9c
Dipen Chauhan <get.dipen@gmail.com>
2018-06-04 16:31:59 -0700

    Defined member variables and functions

--- bank_total.h
+++ bank_total.h
@@ -1,7 +1,39 @@
-class Bank_total
+/**
+ * File Name: bank_total.h
+ * Author: Dipen Chauhan (https://github.com/overdrivemachines)
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+class BankTotal
 {
+	unsigned long int ones;
+	unsigned long int fives;
+	unsigned long int twenty_fives;
+	unsigned long int hundreds;
+	unsigned long int two_hundred_fifties;
+	string bank_server;
+	string status;
+	string time;
+
 public:
-	Bank_total();
-	~Bank_total();
+	BankTotal();
+	BankTotal(unsigned long int ones,
+				unsigned long int fives,
+				unsigned long int twenty_fives,
+				unsigned long int hundreds,
+				unsigned long int two_hundred_fifties);
+	~BankTotal();
+
+	unsigned long int 	get_ones() const;
+	void 				set_ones(const unsigned long int &value);
+	unsigned long int 	get_fives() const;
+	void 				set_fives(const unsigned long int &value);
+	unsigned long int 	get_twenty_fives() const;
+	void 				set_twenty_fives(const unsigned long int &value);
+	unsigned long int 	get_hundreds() const;
+	void 				set_hundreds(const unsigned long int &value);
+	unsigned long int 	get_two_hundred_fifties() const;
+	void 				set_two_hundred_fifties(const unsigned long int &value);
 	
 };
