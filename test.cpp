f09a6439e6446ec55d16bf9c1690680f9781e28e
Dipen Chauhan <get.dipen@gmail.com>
2018-06-04 15:47:19 -0700

    Bank keys test program

--- test.cpp
+++ test.cpp
@@ -1,5 +1,24 @@
-int main(int argc, char const *argv[])
+/**
+ * File Name: test.cpp
+ * Author: Dipen Chauhan (https://github.com/overdrivemachines)
+ */
+#include "bank_keys.h"
+using namespace std;
+
+int main()
 {
-	/* code */
+	BankKeys bk;
+	bk.set_public_key("7cc454a9");
+	bk.set_private_key("1a23b4c5");	
+	bk.set_email("hello@helloworld.com");
+
+	BankKeys bk2("1234", "4567", "test@test.com");
+
+	cout << bk.get_public_key() << "\n";
+	cout << bk.get_private_key() << "\n";
+	cout << bk.get_email() << "\n";
+
+	bk2.to_s();
+
 	return 0;
 }
